ec5c44afadb883980614662a4e8963c5a2e1d7f3
Murali <muralisurya2004@gmail.com>
2025-09-19 00:15:45 +0530

    Create parameters1.js

---
+++ parameters1.js
@@ -0,0 +1,15 @@
+function square(heigth,width)
+{
+console.log( "heigth =" +heigth)
+console.log("width = " +width)
+
+}
+square(10,20)
+
+function rec(height,width,thick)
+{
+    console.log( "heigth =" +height)
+console.log("width = " +width)
+console.log( "thick =" +thick)
+}
+rec(10,12,17)
