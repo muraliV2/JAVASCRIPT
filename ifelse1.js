612608cef3db9cfe18f58a05cf6dfe825d7b9c61
Murali <muralisurya2004@gmail.com>
2025-09-19 23:21:53 +0530

    Create ifelse1.js

---
+++ ifelse1.js
@@ -0,0 +1,17 @@
+
+var climate = "rain"
+if(climate == "spring")
+{
+    console.log("enjoy the booming flowers")
+}
+else if(climate == "summer")
+{
+    console.log("enjoy with the sun")
+}
+else if(climate == "autumn")
+{
+    console.log("Admire the colorful flowers")
+}
+else{
+    console.log("enjoy the rain")
+}
