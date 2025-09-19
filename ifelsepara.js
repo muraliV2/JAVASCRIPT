1624441e892741ba2800fa004e4427c6a4db5e83
Murali <muralisurya2004@gmail.com>
2025-09-19 23:23:55 +0530

    Create ifelsepara.js

---
+++ ifelsepara.js
@@ -0,0 +1,19 @@
+function hi(climate)
+{
+if(climate == "spring")
+{
+    console.log("enjoy the booming flowers")
+}
+else if(climate == "summer")
+{
+    console.log("enjoy with the sun")
+}
+else if(climate == "autumn")
+{
+    console.log("Admire the colorful flowers")
+}
+else{
+    console.log("enjoy the rain")
+}
+}
+hi("summer")
