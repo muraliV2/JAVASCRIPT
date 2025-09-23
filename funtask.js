86d7dbba01bce017dedc709a050d5afac5a0492d
Murali <muralisurya2004@gmail.com>
2025-09-24 00:27:26 +0530

    Create funtask.js

---
+++ funtask.js
@@ -0,0 +1,27 @@
+<style>
+    *
+    {
+        background-color: cornflowerblue;
+    }
+</style>
+<center><h1>WHO is the winner of 2025 ballond-or</h1>
+<button onclick="dem()">DEMBELE</button> &nbsp; &nbsp; 
+<button onclick="yamal()">LAMINE PERUMAL</button>
+<p id="hi">winner of 2025 ballond-or</p>
+</center>
+
+
+<script>
+    var result =  document.getElementById("hi")
+
+    function dem()
+    {
+        result.textContent = "real winner : ousmanae"
+    }
+
+    function yamal()
+    {
+        result.textContent ="foke winner"
+        alert("he will win in future")
+    }
+</script>
