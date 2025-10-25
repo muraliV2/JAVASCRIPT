4bebae2c61810f27dc11eb34212ad73be33340e9
Murali <muralisurya2004@gmail.com>
2025-10-25 23:27:17 +0530

    Create userinput.js

---
+++ userinput.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label for="">enter name</label>
+    <input type="text" placeholder="enter your name" id="hi">
+    <button>submit</button>
+
+  <script src="./index.js"></script>
+</body>
+</html>
