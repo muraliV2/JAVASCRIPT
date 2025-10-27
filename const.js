2268c98c4c7eee9505a3690d265ec716f6da7d19
Murali <muralisurya2004@gmail.com>
2025-10-28 00:32:32 +0530

    Create const.js

---
+++ const.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="./constants.js"></script>
+</body>
+</html>
+
+const hi = 10
+console.log(hi,typeof(hi))
+
+
+the value for hi is set as constant 
+if we try to reinitialize an const variable we end in a error
