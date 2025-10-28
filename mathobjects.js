456c851b211449f1a15c01465fb9d00943986d03
Murali <muralisurya2004@gmail.com>
2025-10-28 11:36:40 +0530

    Create mathobjects.js

---
+++ mathobjects.js
@@ -0,0 +1,18 @@
+let x = 2
+let y = 3
+//console.log(Math.round(x))
+//console.log(Math.floor(x))
+//console.log(Math.ceil(x))
+//console.log(Math.trunc(x))
+//console.log(Math.pow(x,y))
+//console.log(Math.max(x,y))
+//console.log(Math.min(x,y))
+//console.log(Math.sqrt(81))
+//console.log(Math.abs(x))
+console.log(Math.sin(x))
+console.log(Math.cos(x))
+console.log(Math.tan(x))
+console.log(Math.sign(x))
+
+floor only goes down if 2.6 it rounds to 2
+ceil only goes up even if there is 2.1 it rounds it as 3
