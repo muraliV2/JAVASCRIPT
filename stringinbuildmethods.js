07f64e0837db1bbc6866677ae090b686a755c2bc
Murali <muralisurya2004@gmail.com>
2025-10-29 00:04:36 +0530

    Create stringinbuildmethods.js

---
+++ stringinbuildmethods.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+   /* charAt()
+    length()
+    indexof()
+    lastindex()
+    touppercase()
+    tolowercase()
+    trim()
+    repeat()
+    startswith() & endswith() returns bolean value
+    includes()a
+    replaceall('-' , "")
+    padstart(15 ,"0")
+    padends(20"0") */
+
+    let hi = "hello world"
+    console.log(hi.charAt(5))
+    console.log(hi.length-1)
+    console.log(hi.indexOf("l"))
+    console.log(hi.lastIndexOf("l"))
+    console.log(hi.toUpperCase())
+    console.log(hi.toLowerCase())
+    console.log(hi.trim())
+    console.log(hi.repeat(5))
+    console.log(hi.replaceAll("l", "m"))
+    console.log(hi.startsWith("h"))
+    console.log(hi.endsWith("d"))
+    console.log(hi.includes("l"))
+    console.log(hi.padStart(20,"p"))
+    console.log(hi.padEnd(20,"p"))
+</script>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
