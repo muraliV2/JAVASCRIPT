25ac4ef454e0d9bc9370c0faa51201ddbdb5abc6
Murali <muralisurya2004@gmail.com>
2025-10-29 00:05:07 +0530

    Create switchcase.js

---
+++ switchcase.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    let marks = 25
+    let grade ;
+    switch(true)
+    {
+        case marks>=90 :
+             grade = 'A'
+             console.log(grade)
+             break;
+      case marks>=80 :
+             grade = 'B'
+             console.log(grade)
+             break;
+     case marks>=70 :
+             grade = 'C'
+             console.log(grade)
+             break;
+      case marks>=60 :
+             grade = 'D'
+             console.log(grade)
+             break;
+        case marks>=40 :
+             grade = 'E'
+             console.log(grade)
+             break;
+             default :
+             console.log("why did you write the exam bro rewrite ")
+
+
+    }
+</script>
+</html>
