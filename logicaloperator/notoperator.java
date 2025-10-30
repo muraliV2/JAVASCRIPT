9bb79b01a34aef1831c4da548d9458cb6cf8a7b8
Murali <muralisurya2004@gmail.com>
2025-10-31 00:37:10 +0530

    Add HTML document with voting eligibility script

---
+++ logicaloperator/notoperator.java
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    let age = false
+
+    if (!age)
+{
+    console.log("eligible to vote")
+}
+else
+{
+    console.log("not eligibel to vote")
+}
+</script>
+</html>
