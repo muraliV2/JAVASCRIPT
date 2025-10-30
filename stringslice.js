060d5f6306c9bbb5a94177f70826ce3dcb4caddd
Murali <muralisurya2004@gmail.com>
2025-10-30 22:41:35 +0530

    Create stringslice.js

---
+++ stringslice.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    // string slice is used to slice a part of word from the entire word
+
+
+    let fullname = 'lionel messi'
+    let firstname = fullname.slice(0, 6)
+    let lastname = fullname.slice(6,12)
+    console.log(`${firstname}  "and his full name is " ${fullname}` )
+    console.log(lastname)
+</script>
+</html>
+
+i actually saved this as js because i used script tag inside an html filee
+instead of creating more files i have finished in same file itself
