a9e63eed1e51ddc98d92029d018306e699808aab
Murali <muralisurya2004@gmail.com>
2025-11-06 23:02:32 +0530

    Create nestedfunction().js

---
+++ functions/nestedfunction().js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    function job()
+    {
+        console.log("you are hired")
+        function intern()
+        {
+console.log("hired 6 months for intern")
+        }
+        intern()
+    }
+job()
+</script>
+</html>
