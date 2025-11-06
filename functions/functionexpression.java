ac44653cfce8ece982a3d2167cf552daa36c09b4
Murali <muralisurya2004@gmail.com>
2025-11-06 23:00:27 +0530

    Create functionexpression.java

---
+++ functions/functionexpression.java
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+          
+</body>
+<script>
+    let a = function(a,b)
+    {
+        return a + b;
+    }
+   console.log( a(10,20))
+
+       let b = function(a,b)
+    {
+        return a + b;
+    }
+   console.log( a(100,200))
+</script>
+</html>
