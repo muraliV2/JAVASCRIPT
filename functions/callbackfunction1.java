9e882e59a6a51adee50e4648fe1ab5e4e7a6ad5c
Murali <muralisurya2004@gmail.com>
2025-11-06 23:00:07 +0530

    Create callbackfunction1.java

---
+++ functions/callbackfunction1.java
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    function aswin(a,b)
+    {
+        console.log("i am ashwin")
+        a()
+        b()
+    }
+    aswin( function pooja()
+     {
+        console.log("aswin pooja")
+
+     }, function archana()
+     {
+console.log("ashwin archana")
+     })
+</script>
+</html>
