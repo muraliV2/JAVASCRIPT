8d99d99ec1dc24dc5ab05ddd8074db94d21cfe14
Murali <muralisurya2004@gmail.com>
2025-11-06 23:02:07 +0530

    Create namedfunction.js

---
+++ functions/namedfunction.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+
+<script>
+    function a ()
+    {
+        console.log(10)
+         console.log(20)
+          console.log(30)
+    }
+    a()
+</script>
+</html>
