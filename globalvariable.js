bcba4fc15a1e9dd1b6658c10890d64e6b12fb2d5
Murali <muralisurya2004@gmail.com>
2025-11-16 23:52:38 +0530

    Create globalvariable.js

---
+++ globalvariable.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    let x = 1
+
+    function2()
+
+  function function1()
+    {
+        console.log(x)
+    }
+     function function2()
+    {
+        console.log(x)
+    }
+</script>
+</html>
