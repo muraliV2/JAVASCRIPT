773c9041925644f5f1c22827eb9ed768e6ed916d
Murali <muralisurya2004@gmail.com>
2025-11-19 23:35:08 +0530

    Create documentbasics.js

---
+++ documentbasics.js
@@ -0,0 +1,17 @@
+ let hi = arr.map(a => console.log(a))
+    console.log(hi)
+
+    //filter
+    let gi = arr.filter(a=> a>5)
+    console.log(gi)
+
+    console.log(window.screen.height)
+    console.log(window.screen.width)
+    console.log(window.location.href)
+
+console.log(navigator.cookieEnabled)
+console.log(navigator.appCodeName)
+console.log(navigator.platform)
+
+let hie = window.prompt("how are you")
+console.log(hie)
