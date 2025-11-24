d82a311c439c48ca70a1be6d3c269e89a49c72fd
Murali <muralisurya2004@gmail.com>
2025-11-24 23:48:56 +0530

    Create expoertingarray.js

---
+++ module/expoertingarray.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script type="module" src="./module.js"></script>
+</html>
+
+import arr from "./module1.js";
+//from mosule.js
+arr
+console.log(arr)
+
+//from module1.js
+ let arr =[1,2,3,4,5,6,7,8,9]
+
+ export default arr
