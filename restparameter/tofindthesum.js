cd43719ff647970c84dd622c31adb9617f53cd85
Murali <muralisurya2004@gmail.com>
2025-11-26 01:08:33 +0530

    Create tofindthesum.js

---
+++ restparameter/tofindthesum.js
@@ -0,0 +1,12 @@
+function sum(...numbers)
+{
+    let result =0;
+    for(number of numbers)
+    {
+        result+=number;
+    }
+    return result
+}
+
+let add1 = sum(1,2,3,4,5)
+console.log("sum of " +add1)
