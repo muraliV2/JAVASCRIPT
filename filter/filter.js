4521cd499a2ac59089b0b8cc17af69684717abb6
Murali <muralisurya2004@gmail.com>
2025-12-01 23:36:16 +0530

    Create filter.js

---
+++ filter/filter.js
@@ -0,0 +1,12 @@
+}
+let num = [1,2,3,4,5,6,56,78]
+let hi = num.filter(vote)
+console.log(hi)
+function vote(element)
+{
+    return element > 18
+}
+
+
+
+//filter is a validation iterate every element and return if it is true and doesnt return if it is false
