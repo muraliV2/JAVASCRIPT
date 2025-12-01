6f1446c1b6b7e4e303043c98d281d89a3e703cd2
Murali <muralisurya2004@gmail.com>
2025-12-01 22:38:34 +0530

    Create map.js

---
+++ map/map.js
@@ -0,0 +1,12 @@
+let num = [1,2,3,4,5,6]
+let hi = num.map(square)
+console.log(hi)
+function square(element)
+{
+   return  Math.pow(element,3)
+  
+}
+
+
+
+//map is nothing but iterating every element ans store in new array
