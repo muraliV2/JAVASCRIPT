5fadf8dfc80916d465af0897b8a74f8544a6e134
Murali <muralisurya2004@gmail.com>
2025-12-10 23:29:09 +0530

    Create object destructuring.js

---
+++ object destructuring.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "murali",
+    age: 25,
+    FavoriteFood: "briyani",
+};
+
+let { name, age, FavoriteFood } = person;
+
+console.log(name);        // murali
+console.log(age);         // 25
+console.log(FavoriteFood); // briyani
