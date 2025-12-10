9893d7b4bc9e69da27c161de3ad0db46b0c2f0ec
Murali <muralisurya2004@gmail.com>
2025-12-11 01:23:50 +0530

    Create constructoprsinjs.js

---
+++ constructoprsinjs.js
@@ -0,0 +1,10 @@
+function car(make,cc,gear,seat)
+{
+this.make = make,
+this.cc = cc,
+this.gear = gear,
+this.seat = seat
+}
+const car1 =  new car(2011,"1200cc","6Speed","7 seater")
+
+console.log(car1)
