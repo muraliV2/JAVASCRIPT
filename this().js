ff807a462515d77f0e2f6395b324c8db2c45052a
Murali <muralisurya2004@gmail.com>
2025-12-27 22:29:02 +0530

    Create this().js

---
+++ this().js
@@ -0,0 +1,9 @@
+let person =
+{
+    name : "MURALI",
+    age : 25,
+    status :  "unemployed",
+    skills: "fullstack",
+    hrlo : function(){console.log(`my name is ${this.name} and i am  ${this.age} old and i am currently ${this.status}` )}
+}
+person.hrlo();
