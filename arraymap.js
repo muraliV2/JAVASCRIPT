a978e43a892c6d98dc72c1b890f78d29fd8aee53
Murali <muralisurya2004@gmail.com>
2025-12-29 23:10:11 +0530

    Create arraymap.js

---
+++ arraymap.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+     let arr = [1,2,3,4,5,6]
+    // let hi = arr.map(a => a);
+
+    // arr.map(n => console.log(n))
+    // console.log(hi)
+
+    //filter
+
+    let hi = arr.map(a => console.log(a))
+    console.log(hi)
+
+    //filter
+    let gi = arr.filter(a=> a>5)
+    console.log(gi)
+
+
+
+let hie = window.prompt("how are you")
+console.log(hie)
+</script>
+</html>
