a71d11c824f4c7d0ad97180b78f2784f0fc1f187
Murali <muralisurya2004@gmail.com>
2026-05-15 21:11:18 +0530

    Update filter.js

--- filter/filter.js
+++ filter/filter.js
@@ -4,7 +4,7 @@ let hi = num.filter(vote)
 console.log(hi)
 function vote(element)
 {
-    return element > 18
+return element > 18
 }
 //filter is a validation iterate every element and return if it is true and doesnt return if it is false
 //filter
